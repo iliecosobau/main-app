4266c5248f3ed2b824f083b7b1ae7b985d8f79cd
iliecosobau <cosminilie5292@gmail.com>
2026-01-23 07:48:09 -0800

    Add files via upload

---
+++ myapp-main/utils.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func hashpasswords(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	return string(bytes), err
+}
+
+func checkPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+func generateToken(lenght int) string {
+	bytes := make([]byte, lenght)
+	if _, err := rand.Read(bytes); err != nil {
+		log.Fatalf("Failed to generate token : %v", err)
+	}
+	return base64.URLEncoding.EncodeToString(bytes)
+}
